4ce56cf7f4e14abc304d1716ca1b7c76ba8d00a6
kokweikhong <kokweikhong@gmail.com>
2022-07-18 21:44:30 +0800

    add bursa index keys

---
+++ keys/bursa.go
@@ -0,0 +1,36 @@
+package keys
+
+type BURSA_INDEX string
+
+const (
+	CONSUMER_PRODUCTS_AND_SERVICES   BURSA_INDEX = "0001I"
+	INDUSTRIAL_PRODUCTS_AND_SERVICES BURSA_INDEX = "0002I"
+	CONSTRUCTION                     BURSA_INDEX = "0003I"
+	TECHNOLOGY                       BURSA_INDEX = "0005I"
+	FINANCIAL_SERVICES               BURSA_INDEX = "0010I"
+	PROPERTY                         BURSA_INDEX = "0020I"
+	PLANTATION                       BURSA_INDEX = "0025I"
+	REIT                             BURSA_INDEX = "0050I"
+	ENERGY                           BURSA_INDEX = "0061I"
+	HEALTH_CARE                      BURSA_INDEX = "0062I"
+	TELECOMMUNICATIONS_AND_MEDIA     BURSA_INDEX = "0063I"
+	TRANSPORTATION_AND_LOGISTICS     BURSA_INDEX = "0064I"
+	UTILITIES                        BURSA_INDEX = "0065I"
+	FBMKLCI                          BURSA_INDEX = "0200I"
+	FBMAPUSD                         BURSA_INDEX = "0827I"
+	FBMAPMYR                         BURSA_INDEX = "0828I"
+	FBMPALMOIL_NC                    BURSA_INDEX = "0829I"
+	F4GBM                            BURSA_INDEX = "0840I"
+	F4GBMS                           BURSA_INDEX = "0841I"
+	FBMT100                          BURSA_INDEX = "0861I"
+	FBM70                            BURSA_INDEX = "0863I"
+	FBMSCAP                          BURSA_INDEX = "0864I"
+	FBMEMAS                          BURSA_INDEX = "0865I"
+	FBMFLG                           BURSA_INDEX = "0866I"
+	FA40                             BURSA_INDEX = "0867I"
+	FBMSHA                           BURSA_INDEX = "0868I"
+	FBMHIJRAH                        BURSA_INDEX = "0869I"
+	FBMACE                           BURSA_INDEX = "0871I"
+	FBMMSCAP                         BURSA_INDEX = "0872I"
+	FBMMSCS                          BURSA_INDEX = "0873I"
+)
