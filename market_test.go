7cb64f69a87d961b2ec24350184b34ce4927ab38
kokweikhong <kokweikhong@gmail.com>
2022-07-18 21:44:06 +0800

    get market info

--- market_test.go
+++ market_test.go
@@ -3,13 +3,22 @@ package klse_test
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 	"testing"
 
 	klse "github.com/kokweikhong/klsescreener-scraper"
 )
 
 func TestGetMarketInformation(t *testing.T) {
-    market := klse.GetMarketInformation()
-    b, _ := json.MarshalIndent(market, "", "  ")
-    fmt.Println(string(b))
+	market := klse.GetMarketInformation()
+	b, _ := json.MarshalIndent(market, "", "  ")
+	fmt.Println(string(b))
+	for _, v := range market.BursaIndex {
+		idx := strings.Split(v.Link, "/")
+		name := strings.ReplaceAll(v.Name, "&", "AND")
+        name = strings.ReplaceAll(name, "/", "_")
+		name = strings.ReplaceAll(name, " ", "_")
+		name = strings.ToUpper(name)
+		fmt.Printf("%s BURSA_INDEX = \"%s\"\n", name, idx[len(idx)-1])
+	}
 }
